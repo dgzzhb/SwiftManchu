57fce65bd119119fbb98aa83c2b88dcc086cda84
dgzzhb <TianZheng@cmu.edu>
2015-02-27 18:13:07 -0500

    search bar and list

---
+++ SwiftManchu/SwiftManchu/Candy.swift
@@ -0,0 +1,14 @@
+//
+//  Candy.swift
+//  SwiftManchu
+//
+//  Created by Tian Zheng on 2/27/15.
+//  Copyright (c) 2015 Tian Zheng. All rights reserved.
+//
+
+import Foundation
+
+struct Candy {
+    let category : String
+    let name : String
+}
---
+++ SwiftManchu/SwiftManchu/DictTableTableViewController.swift
@@ -0,0 +1,117 @@
+//
+//  DictTableTableViewController.swift
+//  SwiftManchu
+//
+//  Created by Tian Zheng on 2/27/15.
+//  Copyright (c) 2015 Tian Zheng. All rights reserved.
+//
+
+import UIKit
+
+class DictTableTableViewController: UITableViewController {
+    var candies = [Candy]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Sample Data for candyArray
+        self.candies = [Candy(category:"Chocolate", name:"chocolate Bar"),
+            Candy(category:"Chocolate", name:"chocolate Chip"),
+            Candy(category:"Chocolate", name:"dark chocolate"),
+            Candy(category:"Hard", name:"lollipop"),
+            Candy(category:"Hard", name:"candy cane"),
+            Candy(category:"Hard", name:"jaw breaker"),
+            Candy(category:"Other", name:"caramel"),
+            Candy(category:"Other", name:"sour chew"),
+            Candy(category:"Other", name:"gummi bear")]
+        
+        // Reload the table
+        self.tableView.reloadData()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        // #warning Potentially incomplete method implementation.
+//        // Return the number of sections.
+//        return 0
+//    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return self.candies.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //ask for a reusable cell from the tableview, the tableview will create a new one if it doesn't have any
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        
+        // Get the corresponding candy from our candies array
+        let candy = self.candies[indexPath.row]
+        
+        // Configure the cell
+        cell.textLabel!.text = candy.name
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
