b54418cdaeee21fa2dbc4c15c9a0d5ae9a7053c1
dgzzhb <TianZheng@cmu.edu>
2015-06-03 17:33:51 -0400

    remove unnecessary comments

--- SwiftManchu/SwiftManchu/DictTableViewController.swift
+++ SwiftManchu/SwiftManchu/DictTableViewController.swift
@@ -27,11 +27,6 @@ class DictTableViewController : UITableViewController, UISearchBarDelegate, UISe
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     /*
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
      // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -83,11 +78,6 @@ class DictTableViewController : UITableViewController, UISearchBarDelegate, UISe
         self.filterContentForSearchText(searchString)
         return true
     }
-    
-//    func searchDisplayController(controller: UISearchDisplayController!, shouldReloadTableForSearchScope searchOption: Int) -> Bool {
-//        self.filterContentForSearchText(self.searchDisplayController!.searchBar.text)
-//        return true
-//    }
 
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         self.performSegueWithIdentifier("wordDetail", sender: tableView)
@@ -155,40 +145,5 @@ class DictTableViewController : UITableViewController, UISearchBarDelegate, UISe
         
         return result
     }    
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
 }
